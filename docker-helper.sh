11d4f90190a790cf5f62bc15bd7b178a2ac75356
Dimitrie David <david.dimitrie@gmail.com>
2022-10-22 21:45:53 +0300

    initial config done

--- docker-helper.sh
+++ docker-helper.sh
@@ -1,20 +1,39 @@
+#!/bin/bash
+
 # defines
 IMAGE_NAME="ddavid-web"
 VERSION=1.0.0
 
+build () {
+  docker build -t registry.digitalocean.com/ddavid/$IMAGE_NAME:$VERSION -t registry.digitalocean.com/ddavid/$IMAGE_NAME:latest .
+}
+
+run () {
+  docker run --rm -it -p 3001:3001 registry.digitalocean.com/ddavid/$IMAGE_NAME:$VERSION
+}
+
+push () {
+  docker push --all-tags registry.digitalocean.com/ddavid/$IMAGE_NAME
+}
+
+update-server () {
+  ssh root@ddavid.me -t 'docker pull registry.digitalocean.com/ddavid/ddavid-web; cd ./config/ddavid-web/; docker-compose up -d --force-recreate --no-deps ddavid-web'
+}
+
 # if first argument is build
 if [ "$1" = "build" ]; then
-    # build the image
-    docker build -t registry.digitalocean.com/ddavid/$IMAGE_NAME:$VERSION -t registry.digitalocean.com/ddavid/$IMAGE_NAME:latest .
-    # if second argument is run
+    build
 elif [ "$1" = "run" ]; then
-    # run the image
-    docker run --rm -it -p 3001:3001 registry.digitalocean.com/ddavid/$IMAGE_NAME:$VERSION
-    # if second argument is push
+    run
 elif [ "$1" = "push" ]; then
-    # run the image
-    docker push --all-tags registry.digitalocean.com/ddavid/$IMAGE_NAME
+    push
+elif [ "$1" = "update-server" ]; then
+    update-server
+elif [ "$1" = "publish" ]; then
+    build
+    push
+    update-server    
 else
     # print usage
-    echo "Usage: $0 [build|run|push]"
+    echo "Usage: $0 [build|run|push|update-server|publish]"
 fi
