11f3a88060110c977d9a9011073fd575c773ac74
Dimitrie David <david.dimitrie@gmail.com>
2022-10-22 23:36:15 +0300

    added sharks

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const router = express.Router();
+
+const path = __dirname + '/views/';
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/',function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks',function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
